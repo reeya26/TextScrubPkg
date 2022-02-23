bb09b6206eed1c40531b1cf6f0fc45c4c5662628
reeya26 <reeya26@gmail.com>
2022-02-22 22:40:13 -0800

    Update nlp.py

--- textscrub/nlp.py
+++ textscrub/nlp.py
@@ -1,1 +1,8 @@
 
+  """
+    Data Preprocessing Required for NLP Tasks
+    Tokenization, Stop Word Removal, Acronym Expansion, Spell Correction, Lemmatization
+    Args:
+        text(str): raw text
+    return args:
+    """
