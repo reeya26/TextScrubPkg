2e43ae7b4ac17a8170669a29ae3c711621545b50
ree <reeya26@gmail.com>
2022-03-04 21:37:06 -0800

    Updated NLP file with Base functions

--- textscrub/nlp.py
+++ textscrub/nlp.py
@@ -1,8 +1,124 @@
 
-  """
+#NLP Tasks for Software Design for Data Science
+import nltk
+
+#Libraries for Work Tokenization
+nltk.download('punkt')
+from nltk.tokenize import word_tokenize
+
+#Libraries for Stop Word Removal
+import spacy
+#Run the following code below on Terminal
+# $python -m spacy download en_core_web_sm
+#from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
+from autocorrect import Speller
+import spacy
+nltk.download('wordnet')
+from nltk.stem import WordNetLemmatizer
+
+#Libraries for Working with Emojis
+from emoji.core import demojize
+import re
+import emoji
+
+
+"""
     Data Preprocessing Required for NLP Tasks
     Tokenization, Stop Word Removal, Acronym Expansion, Spell Correction, Lemmatization
     Args:
         text(str): raw text
     return args:
-    """
+"""
+
+def tokenizing(text):
+
+  """
+    Splitting the text into tokens
+    Args:
+        text(str): raw text
+    return args[]:
+
+  """
+
+	#Tokenization 
+
+  tokens = word_tokenize(text)
+  return tokens
+
+
+def stopword_removal(text):
+
+  """
+    Removing Stop Words (like articles, prepositions, pronouns, conjunctions, etc) 
+    that do not add much information to the text.
+    Args:
+        text(str): raw text
+    return args[]:
+  """
+
+  #Loading the Small Model of English Stop Words from lib spacy
+
+  en = spacy.load('en_core_web_sm')
+  sw_spacy = en.Defaults.stop_words
+
+  #print(sw_spacy)
+
+  words = [word for word in text.split() if word.lower() not in sw_spacy]
+  new_text = " ".join(words)
+
+
+  return new_text
+
+
+
+def acronym_expansion(text):
+
+  """
+    Converting all acronyms into their full forms
+    Args:
+        text(str): raw text
+    return text:
+  """
+
+  en = spacy.load('en_core_web_sm')
+
+  doc = en(text)
+  sentences = [sent.text.strip() for sent in doc.sents]
+  print(sentences)
+
+def remove_emoji(string):
+  """ 
+  Returns the string without the emojis and also the
+  string with text translation of the emoji as a tuple
+  """
+  emoji_pattern = re.compile("["
+                           u"\U0001F600-\U0001F64F" # emoticons
+                           u"\U0001F300-\U0001F5FF" # symbols & pictographs
+                           u"\U0001F680-\U0001F6FF" # transport & map symbols
+                           u"\U0001F1E0-\U0001F1FF" # flags (iOS)
+                           u"\U00002702-\U000027B0"
+                           u"\U000024C2-\U0001F251"
+                           "]+", flags=re.UNICODE)
+  
+  return (emoji_pattern.sub(r'', string), emoji.demojize(string))
+
+
+def lemmat(text):
+  """ 
+    Lemmatize individual tokens
+    Args:
+        text(str): raw text
+    return args[]:
+  """
+
+  lemmatizer = WordNetLemmatizer()
+
+  tokens = word_tokenize(text)
+  new_text = ""
+
+  for t in tokens:
+    new_text = new_text + lemmatizer.lemmatize(t) + " "
+  
+
+  return new_text
+
