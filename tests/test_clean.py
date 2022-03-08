ebfbcf5a70224ea8c77b399062b138d53fe78af2
Pragya Verma <pragyasurendra.verma@gmail.com>
2022-03-07 22:52:58 -0800

    glyphs

--- tests/test_clean.py
+++ tests/test_clean.py
@@ -1,24 +1,31 @@
+import sys,os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from textscrub import clean
 import unittest
 
+
 class TestingBasicCleaning(unittest.TestCase):
-
-    def test_remove_non_ascii(self):
-
-        a = 'Good bye in Swedish is Hej d\xe5'
-        b = 'Good bye in Swedish is Hej d'
-
-        mssg = 'Values are not equal'
-
-        clean_text = clean.remove_non_ascii(a)       
-        self.assertEqual(clean_text, b, mssg)
 
     def test_remove_hyperlinks(self):
 
         a = 'this line contains hyperlink - https://google.com/'
-        b = 'this line contains hyperlink -'
+        b = 'this line contains hyperlink - '
 
         mssg =  'Values are not equal'
 
         clean_text = clean.remove_hyperlinks(a)
         self.assertEqual(clean_text, b, mssg)
+
+    def test_remove_glyphs(self):
+
+        a = '\x00\x01string'
+        b = 'string'
+
+        mssg = 'Values are not equal'
+
+        clean_text = clean.remove_glyphs(a)       
+        self.assertEqual(clean_text, b, mssg)
+
+if __name__ == '__main__':
+    unittest.main()
